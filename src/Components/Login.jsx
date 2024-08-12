83466bf2fa1a60b5fdbcffa2a7129feefc2a7fd2
Jenifer29b <Jenifer29b@gmail.com>
2024-08-12 10:01:38 +0530

    login updated

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     setLoading(true);
     try {
       const response = await fetch(
-        "https://capstonebackend-aifo.onrender.com/api/auth/login",
+        "https://capstonebackend-aifo.onrender.com /api/auth/login",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
