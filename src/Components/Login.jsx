f6a91fb4df01f0f62d88b5c0b3881cda3ad56538
Jenifer29b <Jenifer29b@gmail.com>
2024-08-14 10:00:14 +0530

    small update

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -12,11 +12,14 @@ const Login = () => {
   const handleSubmit = async (values) => {
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:4000/api/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(values),
-      });
+      const response = await fetch(
+        "https://capstonebackend-aifo.onrender.com/api/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(values),
+        }
+      );
 
       const data = await response.json();
       if (response.ok) {
